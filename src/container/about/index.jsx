3ec5c11ba8ac7f4c0e4cd9a7c533c245db2d8879
kiran vignesh <rajkiranvignesh369@gmail.com>
2024-06-06 22:49:30 +0530

    altered about content and style

--- src/container/about/index.jsx
+++ src/container/about/index.jsx
@@ -8,57 +8,57 @@ const About = () => {
             <PageHeader headerText="About Me" />
             <p style={{fontSize:'2.3rem'}}>
                 Well, Let's face it... If you are reading this, then you are either a recruiter or a fellow software engineer. </p><br />
-            <p>
-                If you are not a recruiter 
+            <p style={{fontSize:'2.1rem'}}>
+                If you are 'not' a recruiter 
                 then just skip this page and explore my projects or whatever you are here for, (honestly man...I would rather spend my time 
                 building, learning, playing or watching anime) so just skedaddle and spend your time wisely.
             </p><br /><br /><br /><br /><br />
             <p style={{fontSize:'2.3rem'}}>
-                Now then, If you are a recruiter then buckle up... cuz I got just the thing to explain about myself
+                Now then, If you are a recruiter: buckle up... cuz I got just the thing to explain about myself
             </p><br />
-            <p>
+            <p style={{fontSize:'2.1rem'}}>
                 I am Kiran Vignesh (23yrs at the time of writing this) and let's jump straight to the context, I am not an average software engineer 
-                based on various aspects. (I do not belittle them, it's a wild and vast profession) let me explain why, in a completely honest and straight forward way 
-                by expressing my heart out, so that you could understand my frame of mind and craft your opinion/assumption about me.
+                based on various aspects. (I do not belittle them, it's a wild and vast profession) let me explain why in a way 
+                that makes you understand my frame of mind and craft your opinion/assumption about me.
             </p><br />
-            <p>
-                Backstory - My first ever programming langugae was C and it was during my highschool, I was probably 16 or 17 back then and I was neither good not bad, I survived my CS101 
+            <p style={{fontSize:'2.1rem'}}>
+                Backstory - My first ever programming langugae was C and it was during my highschool, I was probably 16 or 17 back then and I was neither good nor bad, I survived my CS101 
                 classes and scored barely enough to pass my exams. Fast forward 2-3 years and now I am in college majoring in CS,(ironic, isnt it?) at this point I started investing time and effort to learn 
-                JAVA and other frameworks associated with it, It was a stupendous learning curve for me. I actually had fun building basic projects and was on the top 3 in my class 
-                and obliterated 3hrs exams in just an hour and scored the top ranks as well. It was my first major breakthrough in CS and I was glowing with academic knowledge 
+                JAVA and other frameworks associated with it, It was a stupendous learning curve for me. I actually had fun building basic projects and was on the top 3 in my class. 
+                I obliterated 3hr exams in just an hour and scored really good as well. It was my first major breakthrough in CS and I was glowing with academic knowledge 
                 throughout my course duration.
             </p><br />
-            <p>
+            <p style={{fontSize:'2.1rem'}}>
                 Intellect - It's 2022 and I just got my first job as a trainee at Intellect, the salary was not bad for starters, was introduced to a professional environment, worked with a 
                 bunch of seniors who were just incredible mentors to me, learnt that school smart is absolutely not enough at work, and it was a perfect transition from 
-                school to work environment. The truth is, I mastered the programming languages, frameworks, work flows, SDLCs we followed in under 3 months. It was a double 
-                edged sword for me, GOOD in a way that proves my skills and ability to adapt new tech. BAD in a way that I got really comfortable with the environment. My daily life 
-                was completely monotonous, such that my motivation to do anything became regressive. The tasks assigned to me no-longer seemed to be interesting and only consumed time with 
-                little to medium effort. But hey, I was satisfied with the pay and no longer had any work pressure, Hence I 
-                embraced the never ending loop of boredom.
+                school to work environment. Honestly, I mastered the programming languages, frameworks, work flows, SDLCs we followed in under 3 months. It was a double 
+                edged sword for me, GOOD because I am now really fast and efficient at work. BAD because things got boring, there was no longer any challenging tasks. My daily life 
+                was completely monotonous, and at that point of time, I was ignorant of the glorious software engineering world and was stuck as a frameworker rather than a engineer. 
+                But hey, I was satisfied with the pay with minimal work pressure, Hence I embraced boredom.
             </p><br />
-            <p>
-                October 25, 2022 (The day I broke the matrix) - So, it was just another day at work and while I was on my way home, I had a motorcycle accident (My office was around 
-                50kms away and I used to drive there 5 days a week). It wasnt anything serious or life threatening but I shattered my femur and was on WFH for the next couple of months. WFH as a trainee 
-                wasnt great, but my team was supportive and the work load was really low. Now I had enormous amount of time, time to explore new frameworks, concepts, langugaes. I found the motivation and passion
-                that I once lost by simply building projects that I wanted and learning whatever I felt like(I chose react JS), I learnt MERN stack and built a couple of projects. 
+            <p style={{fontSize:'2.1rem'}}>
+                October 25, 2022 (The day I broke the matrix) - So, it was just another day at work and while I was on my way home, I had a motorcycle accident. 
+                It wasnt anything serious or life threatening but I shattered my femur and switched between medical leave and WFH for the next couple of months. WFH as a trainee 
+                was not good, but my team was supportive and the work load was really low. Now I had enormous amount of time to explore new frameworks, concepts, langugaes. I found the motivation and passion
+                that I once lost by simply building projects, exploring new languages and frameworks. Somewhere down the line I learnt MERN stack and built a couple of projects. 
                 After a while, I finally managed to land my first non-trainee role with my preferrend tech stack.
             </p><br />
-            <p>
+            <p style={{fontSize:'2.1rem'}}>
                 The Tann Mann Foundation - An average software engineer would say that working at a start-up will be a nightmare since you will be the frontend, backend, IT, testing, devops etc,. 
-                but I loved every part of it, I didnt actually have to carry everything since we had around 10-15 software engineers in total, grouped based on their skills and mastery. 
+                but I loved every part of it, I didnt actually have to carry everything since we had around 10-15 software engineers in total. 
                 I felt unshackled in a lot of ways, I dont have to raise a ticket to IT for installing software, didnt have to push my code to the testing team, didnt have to wait on other devs 
-                for their response/contribution/approval, and much more. I also had genuine interactions with our CEO and was able to implement my ideas to the product. The best part is, my job 
-                is WFH but occasionally WFO. so, my work-life balance has never been better and I still have time to work on my side projects every now and then. but it all comes with a cost, 
-                the pay is not good and my comapany has fewer clients and none of them were MNCs, once a product is pushed to production then there isnt much to do with support. Hence not much profit/funds involved.
+                for their response/contribution/approval, and much more. The best part is, my job 
+                is WFH. so, my work-life balance has never been better and I still have time to work on my side projects every now and then. but it all comes with a cost, 
+                the pay is not good and my comapany has fewer clients and none of them were big shots, once a product is pushed to production then there is not much to do with support.
             </p><br />
-            <p>
-                In conclusion - Thats pretty much it, this is my life so far and I genuinely enojy building projects and working on challenging environments, I wish to keep learning new tech rather than sticking 
-                with a particular tech stack or framework. My personal objective in life is to become a software engineer at FAANGM and work on revolutionary products that has a significant 
-                impact in people's daily lives. Well, you probably skmimmed through the above blog, if not I express my heartfelt thanks for spending time to know me.
+            <p style={{fontSize:'2.1rem'}}>
+                In conclusion - I genuinely enojy building projects and working on challenging environments, I wish to keep learning new tech rather than being stuck 
+                with a particular tech stack or framework. My personal objective is to become a software engineer at FAANGM and work on revolutionary products that has a significant 
+                impact in people's daily lives. well... Thats pretty much it, this is my life so far and I absolutely love it. If you find this blog or any of my projects interesting, 
+                simply just reach out to me through any means, lets discuss/converse or just craft ideas and bring them to reality.
             </p><br />
-            <p>
-                set your heart ablaze and push yourself beyond your limits, right here, right now -Kyojuro Rengoku
+            <p style={{fontSize:'2.1rem'}}>
+                set your heart ablaze and push yourself beyond your limits -Kyojuro Rengoku 
             </p><br />
         </section>
     )
