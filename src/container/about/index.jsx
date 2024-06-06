e02a83096130283e6e3e8dd0adcd4cbb61495346
kiran vignesh <rajkiranvignesh369@gmail.com>
2024-06-06 22:58:53 +0530

    updated resume

--- src/container/about/index.jsx
+++ src/container/about/index.jsx
@@ -24,24 +24,24 @@ const About = () => {
             <p style={{fontSize:'2.1rem'}}>
                 Backstory - My first ever programming langugae was C and it was during my highschool, I was probably 16 or 17 back then and I was neither good nor bad, I survived my CS101 
                 classes and scored barely enough to pass my exams. Fast forward 2-3 years and now I am in college majoring in CS,(ironic, isnt it?) at this point I started investing time and effort to learn 
-                JAVA and other frameworks associated with it, It was a stupendous learning curve for me. I actually had fun building basic projects and was on the top 3 in my class. 
+                JAVA and other frameworks associated with it, It was a stupendous learning curve for me. I actually had fun building basic projects and was in the top 3 in my class. 
                 I obliterated 3hr exams in just an hour and scored really good as well. It was my first major breakthrough in CS and I was glowing with academic knowledge 
                 throughout my course duration.
             </p><br />
             <p style={{fontSize:'2.1rem'}}>
                 Intellect - It's 2022 and I just got my first job as a trainee at Intellect, the salary was not bad for starters, was introduced to a professional environment, worked with a 
                 bunch of seniors who were just incredible mentors to me, learnt that school smart is absolutely not enough at work, and it was a perfect transition from 
-                school to work environment. Honestly, I mastered the programming languages, frameworks, work flows, SDLCs we followed in under 3 months. It was a double 
+                school to work environment. Honestly, I mastered the programming languages, frameworks, workflows, SDLCs we followed in under 3 months. It was a double 
                 edged sword for me, GOOD because I am now really fast and efficient at work. BAD because things got boring, there was no longer any challenging tasks. My daily life 
-                was completely monotonous, and at that point of time, I was ignorant of the glorious software engineering world and was stuck as a frameworker rather than a engineer. 
+                was completely monotonous, and at that point of time, I was ignorant of the glorious software engineering world and was stuck as a frameworker rather than an engineer. 
                 But hey, I was satisfied with the pay with minimal work pressure, Hence I embraced boredom.
             </p><br />
             <p style={{fontSize:'2.1rem'}}>
                 October 25, 2022 (The day I broke the matrix) - So, it was just another day at work and while I was on my way home, I had a motorcycle accident. 
                 It wasnt anything serious or life threatening but I shattered my femur and switched between medical leave and WFH for the next couple of months. WFH as a trainee 
-                was not good, but my team was supportive and the work load was really low. Now I had enormous amount of time to explore new frameworks, concepts, langugaes. I found the motivation and passion
+                was not good, but my team was supportive and the workload was really low. Now I had an enormous amount of time to explore new frameworks, concepts, languages. I found the motivation and passion
                 that I once lost by simply building projects, exploring new languages and frameworks. Somewhere down the line I learnt MERN stack and built a couple of projects. 
-                After a while, I finally managed to land my first non-trainee role with my preferrend tech stack.
+                After a while, I finally managed to land my first non-trainee role with my preferred tech stack.
             </p><br />
             <p style={{fontSize:'2.1rem'}}>
                 The Tann Mann Foundation - An average software engineer would say that working at a start-up will be a nightmare since you will be the frontend, backend, IT, testing, devops etc,. 
@@ -49,12 +49,12 @@ const About = () => {
                 I felt unshackled in a lot of ways, I dont have to raise a ticket to IT for installing software, didnt have to push my code to the testing team, didnt have to wait on other devs 
                 for their response/contribution/approval, and much more. The best part is, my job 
                 is WFH. so, my work-life balance has never been better and I still have time to work on my side projects every now and then. but it all comes with a cost, 
-                the pay is not good and my comapany has fewer clients and none of them were big shots, once a product is pushed to production then there is not much to do with support.
+                the pay is not good and my company has fewer clients and none of them were big shots, once a product is pushed to production then there is not much to do with support.
             </p><br />
             <p style={{fontSize:'2.1rem'}}>
-                In conclusion - I genuinely enojy building projects and working on challenging environments, I wish to keep learning new tech rather than being stuck 
-                with a particular tech stack or framework. My personal objective is to become a software engineer at FAANGM and work on revolutionary products that has a significant 
-                impact in people's daily lives. well... Thats pretty much it, this is my life so far and I absolutely love it. If you find this blog or any of my projects interesting, 
+                In conclusion - I genuinely enjoy building projects and working on challenging environments, I wish to keep learning new tech rather than being stuck 
+                with a particular tech stack or framework. My personal objective is to become a software engineer at FAANGM and work on revolutionary products that have a significant 
+                impact on people's daily lives. well... Thats pretty much it, this is my life so far and I absolutely love it. If you find this blog or any of my projects interesting, 
                 simply just reach out to me through any means, lets discuss/converse or just craft ideas and bring them to reality.
             </p><br />
             <p style={{fontSize:'2.1rem'}}>
