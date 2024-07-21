22aa7b51c4a3ead0433dfdd2b8ef07e863d8956e
kiran vignesh <rajkiranvignesh369@gmail.com>
2024-07-21 20:34:26 +0530

    changed about

--- src/container/about/index.jsx
+++ src/container/about/index.jsx
@@ -6,9 +6,13 @@ const About = () => {
     return (
         <section className="about">
             <PageHeader headerText="Who am I?" />
-            <p style={{ fontSize: '2.1rem' }}>
-                I know who I am. I'm the dude playin' the dude, disguised as another dude!  -RDJ
-            </p><br />
+            <p style={{ fontSize: '2.1rem', textAlign: 'center' }}>
+                I'm just Kiran, anywhere else I'd be a ten<br/>
+                Is it my destiny to live and die a life of Vim superiority?<br/>
+                I'm just Kiran<br/>
+                Where I see love, she sees a friend<br/>
+                What will it take for her to see the man behind the tan and fight for me?
+            </p><br /><br /><br />
             <p style={{ fontSize: '2.1rem' }}>
                 Alright, jokes aside... I am Kiran Vignesh (23, M) and I genuinely like to code and work on projects with a tech stack different from the one I use at work
             </p>
